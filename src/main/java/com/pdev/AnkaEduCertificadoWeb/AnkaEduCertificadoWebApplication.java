bf5eea653187185478e264c98f0481fd7cafb0b3
Pablo Castillo <pablocastillo.5.1992@gmail.com>
2024-05-08 11:37:17 -0500

    Creación de proyecto con un endpoint /public/test

---
+++ src/main/java/com/pdev/AnkaEduCertificadoWeb/AnkaEduCertificadoWebApplication.java
@@ -0,0 +1,13 @@
+package com.pdev.AnkaEduCertificadoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkaEduCertificadoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkaEduCertificadoWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pdev/AnkaEduCertificadoWeb/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.pdev.AnkaEduCertificadoWeb.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/test")
+    public ResponseEntity test(){
+        logger.info("Test endpoint works succesfully!");
+        return new ResponseEntity("AnkaEduCertificadoWeb works!", HttpStatus.OK);
+    }
+
+}
